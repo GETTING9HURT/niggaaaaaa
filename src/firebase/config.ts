190a9b002b2f31dfe28467dd20e3c0ab673b4e07
Aabid <aabid.gets.fatality@gmail.com>
2025-10-27 07:54:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7572899159-77389",
+  "appId": "1:552369956743:web:ead111b2d4c5f34df21522",
+  "apiKey": "AIzaSyCso1n_68FRIjvaKKMFa3_zbR1HLOYOYvI",
+  "authDomain": "studio-7572899159-77389.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552369956743"
+};
