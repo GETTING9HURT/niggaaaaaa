fd3403d5c84cdc6a2f83a12c827709f1f673e13b
Aabid <aabid.gets.fatality@gmail.com>
2025-10-27 17:03:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
